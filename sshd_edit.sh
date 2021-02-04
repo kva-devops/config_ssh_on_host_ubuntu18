8a00da8a3630811eeb6bc2c30fdda43aaf1b1ac2
kva-devops <76955734+kva-devops@users.noreply.github.com>
2021-02-04 21:17:14 +0400

    Update sshd_edit.sh

--- sshd_edit.sh
+++ sshd_edit.sh
@@ -1,4 +1,5 @@
-sudo sed -ren 's/^(PasswordAuthentication)([[:space:]]+)no/\1\2yes/' /etc/ssh/sshd_config
-sudo sed -ren 's/^(\#)(PubkeyAuthentication)([[:space:]]+)(.*)/\2\3\4/' /etc/ssh/sshd_config
-sudo sed -ren 's/^(\#)(PermitRootLogin)([[:space:]]+)prohibit-password/\2\3yes/' /etc/ssh/sshd_config
-sudo echo 'RSAAuthentication yes' >> /etc/ssh/sshd_config
+sed -ren 's/^(PasswordAuthentication)([[:space:]]+)no/\1\2yes/' /etc/ssh/sshd_config
+sed -ren 's/^(\#)(PubkeyAuthentication)([[:space:]]+)(.*)/\2\3\4/' /etc/ssh/sshd_config
+sed -ren 's/^(\#)(PermitRootLogin)([[:space:]]+)prohibit-password/\2\3yes/' /etc/ssh/sshd_config
+sudo systemctl restart sshd
+sudo systemctl restart ssh
