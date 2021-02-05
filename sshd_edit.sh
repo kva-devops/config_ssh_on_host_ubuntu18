72d814a69e65f5f0057b8e7997bcd2192470cb98
kva-devops <76955734+kva-devops@users.noreply.github.com>
2021-02-05 07:53:08 +0400

    Update sshd_edit.sh

--- sshd_edit.sh
+++ sshd_edit.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sed -ren 's/^(PasswordAuthentication)([[:space:]]+)no/\1\2yes/' /etc/ssh/sshd_config
 sed -ren 's/^(\#)(PubkeyAuthentication)([[:space:]]+)(.*)/\2\3\4/' /etc/ssh/sshd_config
 sed -ren 's/^(\#)(PermitRootLogin)([[:space:]]+)prohibit-password/\2\3yes/' /etc/ssh/sshd_config
